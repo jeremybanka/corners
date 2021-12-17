723e49ddf01f70a94e0fe80a8e06ece52cf2dd25
Jeremy Banka <hello@jeremybanka.com>
2021-12-17 02:02:09 -0800

    🔥 burn template files

--- test/dumb.test.ts
+++
@@ -1,9 +0,0 @@
-import { MyResource } from "../src/MyResource"
-
-describe(`dumb tests`, () => {
-  it(`dumb test`, () => {
-    const resource = new MyResource(1, 2)
-    const result = resource.execute()
-    expect(result).toBeCloseTo(3)
-  })
-})
