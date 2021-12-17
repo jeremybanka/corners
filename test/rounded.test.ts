8894b92dd1a7a859ac5ea0de4d8b5473a3283317
Jeremy Banka <hello@jeremybanka.com>
2021-12-17 02:06:03 -0800

    ❓ add inspection test

---
+++ test/rounded.test.ts
@@ -0,0 +1,8 @@
+import { drawRoundedBox } from "~/utils/svg"
+
+describe(`dumb tests`, () => {
+  it(`dumb test`, () => {
+    const result = drawRoundedBox(10, 10, 1)
+    console.log(result)
+  })
+})
