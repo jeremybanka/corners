a7f854a65a9b3a422aaee31d8155a15cd49bfaba
Jeremy Banka <hello@jeremybanka.com>
2021-12-30 01:46:30 -0800

    🏷  fix type issue

--- src/utils/interpolate.ts
+++ src/utils/interpolate.ts
@@ -1,4 +1,4 @@
-type Interpolate = (from: number, to: number, completionRatio: number) => number
+type Interpolate = (from: number, to: number, completionRatio?: number) => number
 
 export const interpolate: Interpolate = (from, to, completionRatio = 0.5) =>
   from + completionRatio * (to - from)
