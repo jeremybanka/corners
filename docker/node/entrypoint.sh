9d52ff7d76539f0e10b17e8e806b18e3ce84860d
Jeremy Banka <hello@jeremybanka.com>
2022-01-06 01:01:14 -0800

    🔥

--- docker/node/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "/usr/src/app/node_modules" ]; then
-    npm install
-fi
-
-exec "$@"
--- src/samples.ts
+++
@@ -1,7 +0,0 @@
-import corners from "."
-import { chamfer, round } from "./utils/svg"
-
-export const rounded = corners(round).size(20)
-export const chamfered = corners(chamfer).size(20)
-export const semiChamfered = corners(null, chamfer).size(20)
-export const demiChamfered = corners(chamfer, null).size(20)
