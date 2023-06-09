090adacc9772fb67ebfe143cb9ce68a67f566c87
Jeremy Banka <hello@jeremybanka.com>
2023-06-09 00:46:39 -0700

    ✨ export interpolation function

--- src/utils/interpolate.ts
+++ src/utils/interpolate.ts
@@ -1,4 +1,8 @@
-type Interpolate = (from: number, to: number, completionRatio?: number) => number
+export type Interpolate = (
+  from: number,
+  to: number,
+  completionRatio?: number
+) => number
 
 export const interpolate: Interpolate = (from, to, completionRatio = 0.5) =>
   from + completionRatio * (to - from)
