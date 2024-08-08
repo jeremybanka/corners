372494c5e6e36c4b3825c7a8386c97b1f33f4513
Jeremy Banka <hello@jeremybanka.com>
2024-08-07 19:46:04 -0700

    🔧 reconfigure

---
+++ eslint.config.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line quotes
+declare module "eslint-plugin-import" {
+	const plugin: Eslint.Plugin
+	export default plugin
+}
+// eslint-disable-next-line quotes
+declare module "@next/eslint-plugin-next" {
+	const plugin: Eslint.Plugin
+	export default plugin
+}
+// eslint-disable-next-line quotes
+declare module "@typescript-eslint/eslint-plugin" {
+	const plugin: Eslint.Plugin
+	export default plugin
+}
